32dd3e05f58a7cbe2350a4fbebbd7d6bf5af4682
BeatrizFerreira <beatriz.216@hotmail.com>
2015-11-13 12:27:03 -0200

    Initial approach for bluetooth socket connection

--- app/src/main/java/bikeblocker/bikeblocker/Control/ConnectBluetoothActivity.java
+++ app/src/main/java/bikeblocker/bikeblocker/Control/ConnectBluetoothActivity.java
@@ -4,19 +4,29 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.UUID;
 
+import bikeblocker.bikeblocker.Model.App;
 import bikeblocker.bikeblocker.R;
 
 public class ConnectBluetoothActivity extends Activity {
@@ -25,6 +35,9 @@ public class ConnectBluetoothActivity extends Activity {
     private ListView devicesListView;
     private ArrayAdapter<String> adapter;
     private BluetoothDevice bluetoothDevice;
+    private UUID MY_UUID = UUID.fromString("00001101-0000-1000-9000-00905A9B34FB");
+    private InputStream inputStream = null;
+    private BluetoothSocket bluetoothSocket = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +49,30 @@ public class ConnectBluetoothActivity extends Activity {
         devicesListView = (ListView) findViewById(R.id.devicesListView);
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         devicesListView.setAdapter(adapter);
+        devicesListView.setOnItemClickListener(bikeblockerClickListener());
 
         turnBluetoothOn();
+    }
+
+    private AdapterView.OnItemClickListener bikeblockerClickListener() {
+        return new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder  builder = new AlertDialog.Builder(ConnectBluetoothActivity.this);
+
+                builder.setTitle("Connect to BikeBlocker Bluetooth device.");
+                builder.setPositiveButton(R.string.connect, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        connect();
+                        showStartDialog();
+                    }
+                });
+
+                builder.setNegativeButton(R.string.button_cancel, null);
+                builder.show();
+            }
+        };
     }
 
     public void turnBluetoothOn() {
@@ -47,8 +82,6 @@ public class ConnectBluetoothActivity extends Activity {
 
             Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(turnOn, 0);
-
-            Toast.makeText(getApplicationContext(), "Turned on", Toast.LENGTH_LONG).show();
         }
         else {
             Toast.makeText(getApplicationContext(), "Bluetooth is already on", Toast.LENGTH_LONG).show();
@@ -86,6 +119,62 @@ public class ConnectBluetoothActivity extends Activity {
             };
             IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
             registerReceiver(mReceiver, filter);
+        }
+    }
+
+    private void connect(){
+        bluetoothAdapter.cancelDiscovery();
+        try{
+            bluetoothSocket = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(MY_UUID);
+            // Attempt to connect to a remote device.
+            bluetoothSocket.connect();
+            Toast.makeText(getApplicationContext(), "Conectado", Toast.LENGTH_SHORT).show();
+        } catch(IOException e){
+            Log.e("ERRO AO CONECTAR", "O erro foi " + e.getMessage());
+            Toast.makeText(getApplicationContext(), "Conexão não foi estabelecida", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private void showStartDialog(){
+
+    }
+
+    //Colocar isso em uma thread quando nova atividade iniciar
+    private void receberDados(){
+        // Verifica se há conexão estabelecida com o Bluetooth.
+        if(bluetoothSocket != null){
+
+            //textoRecebido.setText("");
+
+            try{
+
+                // Get the input stream associated with this socket.
+                inputStream = bluetoothSocket.getInputStream();
+                int available = inputStream.available();
+                if (available > 0) {
+                    // Cria um buffer para ler a "sujeira"
+                    byte[] uselessBuffer = new byte[available];
+
+                    // Lê a "sujeira"
+                    inputStream.read(uselessBuffer);
+                }
+
+                byte[] msgBuffer = new byte[1];
+
+                // Reads bytes from this stream and stores them in the byte array
+                inputStream.read(msgBuffer);
+
+               // textoRecebido.setText(new String(msgBuffer));
+
+                Toast.makeText(getApplicationContext(), "Mensagem foi recebida", Toast.LENGTH_LONG).show();
+
+            } catch(IOException e){
+                Log.e("ERRO AO RECEBER MENSAGEM", "O erro foi" + e.getMessage());
+                Toast.makeText(getApplicationContext(), "Mensagem não recebida", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Bluetooth não está conectado", Toast.LENGTH_LONG).show();
         }
     }
 
